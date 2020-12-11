e8f3546063cb10a22b3f8fec29f5308b2f1da2b2
Mikhail Samin <i@mishasamin.xyz>
2020-12-11 21:00:01 +0300

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,5 +1,6 @@
 module github.com/AudDMusic/audd-go
 
-go 1.14
+go 1.15
 
 require github.com/bogem/id3v2 v1.2.0 // indirect
+require github.com/Mihonarium/golongpoll/go-client/glpclient v1.2.0
---
+++ longpoll.go
@@ -0,0 +1,57 @@
+package audd
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/Mihonarium/golongpoll/go-client/glpclient"
+	"net/url"
+	"strconv"
+)
+
+const longPollingUrl = MainAPIEndpoint + "longpoll/"
+
+func getMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func (c *Client) getLongPollCategory(RadioID int) string {
+	return getMD5Hash(getMD5Hash(c.ApiToken) + strconv.Itoa(RadioID))[0:9]
+}
+
+type LongPoll struct {
+	stop chan interface{}
+	ResultsChan chan StreamCallback
+}
+
+func (lp *LongPoll) Stop() {
+	lp.stop <- struct {}{}
+}
+
+func (c *Client) ConnectToLongPoll(RadioID int) LongPoll {
+	u, _ := url.Parse(longPollingUrl)
+	lpC := glpclient.NewClient(u, c.getLongPollCategory(RadioID))
+	lpC.LoggingEnabled = false
+	lp := LongPoll{
+		stop:    make(chan interface{}, 1),
+		ResultsChan: make(chan StreamCallback, 1),
+	}
+	go func() {
+		for {
+			select {
+			case e := <-lpC.EventsChan:
+				var song StreamCallback
+				err := json.Unmarshal(e.Data, &song)
+				if err == nil {
+					lp.ResultsChan <- song
+				}
+			case <-lp.stop:
+				lpC.Stop()
+				return
+			}
+		}
+	}()
+	return lp
+}
